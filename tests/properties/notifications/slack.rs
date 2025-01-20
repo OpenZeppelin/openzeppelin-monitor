61592f5a9deb59a703b664686fe1054a1443c97f
shahnami <me@nami.sh>
2025-01-20 16:14:07 +0400

    test: add pbts for notification formatting

---
+++ tests/properties/notifications/slack.rs
@@ -0,0 +1,66 @@
+use openzeppelin_monitor::services::notification::SlackNotifier;
+use proptest::{prelude::*, test_runner::Config};
+use std::collections::HashMap;
+
+fn template_variables_strategy() -> impl Strategy<Value = HashMap<String, String>> {
+	prop::collection::hash_map("[a-zA-Z0-9_]{1,10}", "[a-zA-Z0-9 ]{1,20}", 1..5)
+}
+
+proptest! {
+	#![proptest_config(Config {
+		failure_persistence: None,
+		..Config::default()
+	})]
+
+	#[test]
+	fn test_notification_template_idempotency(
+		template in "[a-zA-Z0-9 ${}_]{1,100}",
+		vars in template_variables_strategy()
+	) {
+		let notifier = SlackNotifier::new(
+			"https://hooks.slack.com/test".to_string(),
+			"Test".to_string(),
+			template.clone()
+		);
+
+		let first_pass = notifier.format_message(&vars);
+		let second_pass = notifier.format_message(&vars);
+
+		prop_assert_eq!(first_pass, second_pass);
+	}
+
+	#[test]
+	fn test_notification_variable_boundaries(
+		template in "[a-zA-Z0-9 ]{0,50}\\$\\{[a-z_]+\\}[a-zA-Z0-9 ]{0,50}",
+		vars in template_variables_strategy()
+	) {
+		let notifier = SlackNotifier::new(
+			"https://hooks.slack.com/test".to_string(),
+			"Test".to_string(),
+			template.clone()
+		);
+
+		let formatted = notifier.format_message(&vars);
+
+		// Verify no partial variable substitutions occurred
+		prop_assert!(!formatted.contains("${{"));
+		prop_assert!(!formatted.contains("}}"));
+	}
+
+	#[test]
+	fn test_notification_empty_variables(
+		template in "[a-zA-Z0-9 ${}_]{1,100}"
+	) {
+		let notifier = SlackNotifier::new(
+			"https://hooks.slack.com/test".to_string(),
+			"Test".to_string(),
+			template.clone()
+		);
+
+		let empty_vars = HashMap::new();
+		let formatted = notifier.format_message(&empty_vars);
+
+		// Template should remain unchanged when no variables are provided
+		prop_assert_eq!(formatted, format!("*Test*\n\n{}", template));
+	}
+}
