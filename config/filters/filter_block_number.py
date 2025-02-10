b955fd62371f37f68c90593709cc1830f83d8113
Nicolas Molina <nicolas.molina@openzeppelin.com>
2025-02-10 13:27:52 -0300

    fix: adding python script examples

---
+++ config/filters/filter_block_number.py
@@ -0,0 +1,13 @@
+def filter_block_number(processed_block: dict) -> bool:
+    """
+    Filter function that returns True if the block number is even.
+    
+    Args:
+        processed_block (dict): A dictionary containing either EVM or Stellar transaction data
+                            with 'block_number' field indicating the block number
+        
+    Returns:
+        bool: True if the block number is even, False otherwise
+    """
+    block_number = processed_block.get('block_number', 0)
+    return block_number % 2 == 0
