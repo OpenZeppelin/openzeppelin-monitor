f9e1072297d7337ffdb0dafcd7a65e4ff35f1728
Nicolas Molina <nicolas.molina@openzeppelin.com>
2025-02-10 15:44:19 -0300

    fix: Add docstring explaining ProcessedBlock dictionary structure

--- config/filters/filter_block_number.py
+++ config/filters/filter_block_number.py
@@ -1,3 +1,10 @@
+"""
+ ProcessedBlock is a dictionary containing either EVM or Stellar transaction data with the following fields:
+    block_number: u64,
+	network_slug: String,
+	processing_results: Array[MonitorMatch],
+"""   
+
 def filter_block_number(processed_block: dict) -> bool:
     """
     Filter function that returns True if the block number is even.
