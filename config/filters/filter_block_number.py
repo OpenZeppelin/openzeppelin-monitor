92ee3586123c4effcb8dfbce94eff4d4b98001fe
Nicolas Molina <nicolas.molina@openzeppelin.com>
2025-02-11 14:06:57 -0300

    feat: PLAT-6148 adding description of input for custom filters example

--- config/filters/filter_block_number.py
+++ config/filters/filter_block_number.py
@@ -1,3 +1,14 @@
+"""
+The processed_block dictionary has the following shape:
+
+{
+    "block_number": u64,  # Block number of the transaction
+    "network_slug": str,  # Network identifier (e.g. "ethereum-mainnet", "polygon-mainnet")
+    "processing_results": [# Array of MonitorMatch objects containing match results]
+}
+
+The filter function below determines whether to trigger alerts based on the block number.
+"""
 def filter_block_number(processed_block: dict) -> bool:
     """
     Filter function that returns True if the block number is even.
