8eb7a40d1293d5e37c9a3364e1d03efaa4b5719a
tirumerla <tirumerla@gmail.com>
2025-03-10 19:16:41 -0700

    fix: Eol

--- config/filters/evm_filter_block_number.sh
+++ config/filters/evm_filter_block_number.sh
@@ -26,7 +26,7 @@ set -e
 
 main() {
     verbose=false
-    
+
     # Read JSON input from stdin
     input_json=$(cat)
 
@@ -84,7 +84,7 @@ main() {
 
     # Check if even or odd using modulo
     is_even=$((block_number % 2))
-    
+
     if [ $is_even -eq 0 ]; then
         echo "Block number $block_number is even"
         echo "Verbose mode: $verbose"
