b44c26af00dad955976a87d09ec5fd03d2f031a2
shahnami <me@nami.sh>
2025-05-13 12:22:21 +0400

    chore: Remove unused From implementation

--- src/models/blockchain/evm/receipt.rs
+++ src/models/blockchain/evm/receipt.rs
@@ -130,15 +130,6 @@ impl From<BaseReceipt> for TransactionReceipt {
 	}
 }
 
-impl From<Vec<BaseLog>> for TransactionReceipt {
-	fn from(logs: Vec<BaseLog>) -> Self {
-		Self(BaseReceipt {
-			logs,
-			..Default::default()
-		})
-	}
-}
-
 impl From<AlloyTransactionReceipt> for TransactionReceipt {
 	fn from(receipt: AlloyTransactionReceipt) -> Self {
 		let inner_receipt = match &receipt.inner {
